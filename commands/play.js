1d880b19255f90f8841675f74d5644f8918791a2
TBMPQF <benjaminsourd@hotmail.fr>
2021-05-10 03:15:34 +0200

    sfd

--- commands/play.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    run: async (message, args) => {
-        message.delete()
-    },
-    name: 'play',
-    help: {description: '𝐉e joue une musique quand tu mets donne une \`URL\` Youtube.'}
-}
