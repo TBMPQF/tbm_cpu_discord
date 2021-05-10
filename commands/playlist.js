dbf5e7fdaf99bba0deb77295e3d92a8e5f783107
TBMPQF <benjaminsourd@hotmail.fr>
2021-05-10 02:16:58 +0200

    ghj

--- commands/playlist.js
+++ commands/playlist.js
@@ -1,15 +1,16 @@
 var list = [];
 
-if(message.content === prefix + "playlist"){
-    let message = "**PLAYLIST :**\n";
-    for(var i = 1;i < list.length;i++){
-    let name;
+module.exports = {
+    run: async (message, args) => {
+        const message = "**PLAYLIST :**\n";
+            for(var i = 1;i < list.length;i++){
+            const name;
 
-    let getinfo = await ytdl.getBasicInfo(list[i]);
-    name = getinfo.videoDetails.title;
-    message += '**' + i + ".** " + name + '\n';
+            const getinfo = await ytdl.getBasicInfo(list[i]);
+            name = getinfo.videoDetails.title;
+            message += '**' + i + ".** " + name + '\n';
 
+            }
+        message.channel.send(message);
     }
-    message.channel.send(message);
-    
-  }
+}
