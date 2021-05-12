9f82407b5b6d40e7b751e8531de6513dcacd28f5
TBMPQF <benjaminsourd@hotmail.fr>
2021-05-13 01:46:40 +0200

    fdg

--- commands/skip.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    run: async (Client, message, args) => {
-        if(!message.member.voice.channel) return message.channel.send("please join a voice channel first!");
-
-        Client.player.skip(message);
-    },
-    name: "skip",
-}
