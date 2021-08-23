efafcfada86a6dde2c1e9889d16567d635a79d2b
TBMPQF <benjaminsourd@hotmail.fr>
2021-08-23 23:47:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ Client.on("message", async message => {
     message.react("831961805705183254")
   }
   if(message.content === "pk"||message.content === "PK"||message.content === "pq"||message.content === "PQ"||message.content === "pk ?"||message.content === "pk?"||message.content === "xd"||message.content === "xD"||message.content === "XD"){
-    message.react("‼️")
+    message.react("🇸")
   }
   if(message.content === "sa va"||message.content === "sa va?"||message.content === "sa va ?"||message.content === "simer"||message.content === "re"||message.content === "RE"||message.content === "Re"||message.content === "cimer"||message.content === "LOL"){
     message.react("813799700092157952")
